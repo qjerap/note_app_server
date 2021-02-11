bfc620d9432aafddce179c8d189092b33424f510
Romain PAREJA <39220340+qjerap@users.noreply.github.com>
2021-02-11 12:58:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://romainpareja:8udfguBxfOPqCR9w@cluster0.zezdb.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://romainpareja:####@cluster0.zezdb.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
