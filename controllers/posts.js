39d2fd8361b344bc2a69196b345045c45886083c
qjerap <romainpareja@gmail.com>
2021-02-11 12:14:58 -0500

    first commit

---
+++ controllers/posts.js
@@ -0,0 +1,32 @@
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const note = req.body.newNote;
+  const newPost = new PostMessage(note);
+
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deleted = await PostMessage.findByIdAndDelete(id);
+    res.status(200).json(`${id} has been deleted`);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
---
+++ models/postMessage.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    creator: String,
+    category: String,
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
+});
+
+const PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
