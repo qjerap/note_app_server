c14b10a384455ad36ce6184ac2b0aff2c8cfe6dc
qjerap <romainpareja@gmail.com>
2021-02-24 09:33:34 -0500

    port

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.get('/', (req, res) => {
     res.send('Welcome')
 });
 
+const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
